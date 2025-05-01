f8af327b17d0449d53cbe33883a16599fb70c0e1
tolga yardimci <tolgayardimci@Mac.lan>
2025-04-30 17:49:59 -0700

    Connected contact and newsletter forms to FormSubmit -3

--- src/components/Contact/NewsLatterBox.tsx
+++ src/components/Contact/NewsLatterBox.tsx
@@ -18,24 +18,27 @@ const NewsLatterBox = () => {
       <form
         action="https://formsubmit.co/info%40lookininc.com"
         method="POST"
+        className="flex flex-col"
       >
+        {/* disable built-in captcha */}
         <input type="hidden" name="_captcha" value="false" />
 
-        <div className="flex items-center">
-          <input
-            type="email"
-            name="email"
-            required
-            placeholder="Your email address"
-            className="border-stroke mr-2 flex-1 rounded-sm border bg-[#f8f8f8] px-4 py-2 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark"
-          />
-          <button
-            type="submit"
-            className="rounded-sm bg-primary px-6 py-2 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
-          >
-            Subscribe
-          </button>
-        </div>
+        {/* Email input */}
+        <input
+          type="email"
+          name="email"
+          required
+          placeholder="Your email address"
+          className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-4 py-2 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark"
+        />
+
+        {/* Subscribe button below */}
+        <button
+          type="submit"
+          className="mt-4 w-full rounded-sm bg-primary px-6 py-2 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
+        >
+          Subscribe
+        </button>
       </form>
     </div>
   );
